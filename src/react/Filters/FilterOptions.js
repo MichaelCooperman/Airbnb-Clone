135377088d95441078ff666990af858716075fa8
Michael <Mich4elcoop321@gmail.com>
2022-12-02 11:45:32 -0500

    test4

--- src/react/Filters/FilterOptions.js
+++ src/react/Filters/FilterOptions.js
@@ -1,17 +1,17 @@
 import { Fragment } from "react";
-// import IMGS from "../../images";
-import Amazingviews from "../../images/filters/Amazingviews.jpg";
-import Bedandbreakfast from "../../images/filters/Bedandbreakfasts.jpg";
-import Cabins from "../../images/filters/Cabins.jpg";
-import CreativeSpaces from "../../images/filters/Creativespaces.jpg";
-import Farms from "../../images/filters/Farms.jpg";
-import Islands from "../../images/filters/Islands.jpg";
-import Mansions from "../../images/filters/Mansions.jpg";
-import Skiing from "../../images/filters/Skiing.jpg";
-import TinyHomes from "../../images/filters/Tinyhomes.jpg";
-import OMG from "../../images/filters/OMG.jpg";
+import IMGS from "../../images";
 import classes from "./filter-options.module.css";
 import requests from "../Cards/requests.js";
+// import Amazingviews from "../../images/filters/Amazingviews.jpg";
+// import Bedandbreakfast from "../../images/filters/Bedandbreakfasts.jpg";
+// import Cabins from "../../images/filters/Cabins.jpg";
+// import CreativeSpaces from "../../images/filters/Creativespaces.jpg";
+// import Farms from "../../images/filters/Farms.jpg";
+// import Islands from "../../images/filters/Islands.jpg";
+// import Mansions from "../../images/filters/Mansions.jpg";
+// import Skiing from "../../images/filters/Skiing.jpg";
+// import TinyHomes from "../../images/filters/Tinyhomes.jpg";
+// import OMG from "../../images/filters/OMG.jpg";
 
 const FilterOptions = ({ setSelectedOption }) => {
   return (
@@ -24,7 +24,8 @@ const FilterOptions = ({ setSelectedOption }) => {
           <span>
             <img
               className={classes.imgs}
-              src={Amazingviews}
+              // src={Amazingviews}
+              src={IMGS.amazingviews}
               alt="Amazingviews-img"
             />
             <div>
@@ -39,7 +40,8 @@ const FilterOptions = ({ setSelectedOption }) => {
           <span>
             <img
               className={classes.imgs}
-              src={Bedandbreakfast}
+              // src={Bedandbreakfast}
+              src={IMGS.bedandbreakfast}
               alt="Bed&Breakfast-img"
             />
             <div>
@@ -52,7 +54,12 @@ const FilterOptions = ({ setSelectedOption }) => {
           onClick={() => setSelectedOption(requests.fetchCabins)}
         >
           <span>
-            <img className={classes.imgs} src={Cabins} alt="Cabins-img" />
+            <img
+              className={classes.imgs}
+              // src={Cabins}
+              src={IMGS.cabins}
+              alt="Cabins-img"
+            />
             <div>
               <span>Cabins</span>
             </div>
@@ -65,7 +72,8 @@ const FilterOptions = ({ setSelectedOption }) => {
           <span>
             <img
               className={classes.imgs}
-              src={CreativeSpaces}
+              // src={CreativeSpaces}
+              src={IMGS.creativespaces}
               alt="CreativeSpaces-img"
             />
             <div>
@@ -78,7 +86,12 @@ const FilterOptions = ({ setSelectedOption }) => {
           onClick={() => setSelectedOption(requests.fetchFarms)}
         >
           <span>
-            <img className={classes.imgs} src={Farms} alt="Farms-img" />
+            <img
+              className={classes.imgs}
+              // src={Farms}
+              src={IMGS.farms}
+              alt="Farms-img"
+            />
             <div>
               <span>Farms</span>
             </div>
@@ -89,7 +102,12 @@ const FilterOptions = ({ setSelectedOption }) => {
           onClick={() => setSelectedOption(requests.fetchIslands)}
         >
           <span>
-            <img className={classes.imgs} src={Islands} alt="Islands-img" />
+            <img
+              className={classes.imgs}
+              // src={Islands}
+              src={IMGS.islands}
+              alt="Islands-img"
+            />
             <div>
               <span>Islands</span>
             </div>
@@ -100,7 +118,12 @@ const FilterOptions = ({ setSelectedOption }) => {
           onClick={() => setSelectedOption(requests.fetchMansions)}
         >
           <span>
-            <img className={classes.imgs} src={Mansions} alt="Mansions-img" />
+            <img
+              className={classes.imgs}
+              // src={Mansions}
+              src={IMGS.mansions}
+              alt="Mansions-img"
+            />
             <div>
               <span>Mansions</span>
             </div>
@@ -111,7 +134,12 @@ const FilterOptions = ({ setSelectedOption }) => {
           onClick={() => setSelectedOption(requests.fetchSkiing)}
         >
           <span>
-            <img className={classes.imgs} src={Skiing} alt="Skiing-img" />
+            <img
+              className={classes.imgs}
+              // src={Skiing}
+              src={IMGS.skiing}
+              alt="Skiing-img"
+            />
             <div>
               <span>Skiing</span>
             </div>
@@ -122,7 +150,12 @@ const FilterOptions = ({ setSelectedOption }) => {
           onClick={() => setSelectedOption(requests.fetchTinyhomes)}
         >
           <span>
-            <img className={classes.imgs} src={TinyHomes} alt="TinyHomes-img" />
+            <img
+              className={classes.imgs}
+              // src={TinyHomes}
+              src={IMGS.tinyhomes}
+              alt="TinyHomes-img"
+            />
             <div>
               <span>Tiny Homes</span>
             </div>
@@ -133,7 +166,12 @@ const FilterOptions = ({ setSelectedOption }) => {
           onClick={() => setSelectedOption(requests.fetchOmg)}
         >
           <span>
-            <img className={classes.imgs} src={OMG} alt="OMG-img" />
+            <img
+              className={classes.imgs}
+              // src={OMG}
+              src={IMGS.omg}
+              alt="OMG-img"
+            />
             <div>
               <span>OMG!</span>
             </div>
