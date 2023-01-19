9a33fbb65507c2338aee9f0320112d93c4a0cbfc
Michael <Mich4elcoop321@gmail.com>
2023-01-19 17:52:47 -0500

    res fixed

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import Filters from "../src/react/Filters/Filters.js";
 import Cards from "../src/react/Cards/Cards.js";
 import requests from "./react/Cards/requests.js";
 import Footer from "./react/Footer/Footer.js";
+import classes from "./react/Header/header.module.css";
 
 function App() {
   const [selectedOption, setSelectedOption] = useState(requests.fetchTrending);
 
   return (
     <React.Fragment>
-      <Header setSelectedOption={setSelectedOption} />
-      <Filters setSelectedOption={setSelectedOption} />
-      <Cards selectedOption={selectedOption} />
-      <Footer />
+      <div className={classes.add}>
+        <div className={classes.res}>
+          <Header setSelectedOption={setSelectedOption} />
+          <Filters setSelectedOption={setSelectedOption} />
+          <Cards selectedOption={selectedOption} />
+          <Footer />
+        </div>
+      </div>
     </React.Fragment>
   );
 }
